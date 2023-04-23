f7914fab6a6bfaa92c4007f7490c8a3ded4bf579
ilyas ipek <infoilyasipek@gmail.com>
2023-04-23 22:49:16 +0300

    Setup theme's colors

--- app/src/main/java/com/example/satellite/ui/theme/Color.kt
+++ app/src/main/java/com/example/satellite/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.satellite.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBackgroundColor = Color(0xFFFAFAFA)
+val LightSurfaceColor = Color(0xFFFFFFFF)
+val LightOnBackgroundColor = Color(0xFF818181)
+val LightOnSurfaceColor = Color(0xFF818181)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueColor = Color(0xFF1E88E5)
